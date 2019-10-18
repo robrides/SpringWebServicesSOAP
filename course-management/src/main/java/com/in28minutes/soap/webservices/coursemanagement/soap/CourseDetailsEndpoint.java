4a2e0a5c46f3bb4656c72b332d121b0f8027fbae
Rob Lounsbury <rob.lounsbury@gmail.com>
2019-10-18 09:36:20 -0500

    Re-established endpoint and app classes. Setup ws config.

--- course-management/src/main/java/com/in28minutes/soap/webservices/coursemanagement/soap/CourseDetailsEndpoint.java
+++ course-management/src/main/java/com/in28minutes/soap/webservices/coursemanagement/soap/CourseDetailsEndpoint.java
@@ -11,17 +11,19 @@ import com.in28minutes.courses.GetCourseDetailsResponse;
 
 @Endpoint
 public class CourseDetailsEndpoint {
-	
-	@PayloadRoot(namespace="http://in28minutes.com/courses", localPart="GetCourseDetailsRequest")
+
+	@PayloadRoot(namespace = "http://in28minutes.com/courses", localPart = "GetCourseDetailsRequest")
 	@ResponsePayload
-	public GetCourseDetailsResponse rocessCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
+	public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
 		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+		
 		CourseDetails courseDetails = new CourseDetails();
 		courseDetails.setId(request.getId());
-		courseDetails.setName("Microservices");
-		courseDetails.setDescription("Great course");
+		courseDetails.setName("Microservices Course");
+		courseDetails.setDescription("That would be a wonderful course!");
 		
 		response.setCourseDetails(courseDetails);
+		
 		return response;
 	}
 
